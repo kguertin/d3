896e724f870ab23900622f1e87b2e86e0e828f8e
Kevin Guertin <kevin.m.guertin@gmail.com>
2022-07-20 13:55:44 -0400

    create svg elements dynamicly

--- basics/index.js
+++ basics/index.js
@@ -6,11 +6,18 @@ const data = [
 
 const svg = d3.select("svg");
 
-const rect = svg
-  .selectAll("rect")
-  .data(data)
+const rects = svg.selectAll("rect").data(data);
+
+// Add attr to rects in DOM
+rects
   .attr("width", (d) => d.width)
   .attr("height", (d) => d.height)
   .attr("fill", (d) => d.fill);
 
-console.log(rect);
+// Append the enter selection to DOM
+rects
+  .enter()
+  .append("rect")
+  .attr("width", (d) => d.width)
+  .attr("height", (d) => d.height)
+  .attr("fill", (d) => d.fill);
