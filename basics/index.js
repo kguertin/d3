7819d927e5665e46266719a39c4def07dbe94b1a
Kevin Guertin <kevin.m.guertin@gmail.com>
2022-07-20 13:22:51 -0400

    create svg using data object

--- basics/index.js
+++ basics/index.js
@@ -1,37 +1,10 @@
-const canvas = d3.select(".canvas");
-const svg = canvas.append("svg").attr("height", 600).attr("width", 600);
+const data = [{ width: 200, height: 400, fill: "purple" }];
 
-const group = svg.append("g").attr("transform", "translate(50, 100)");
+const svg = d3.select("svg");
 
-//Append shapes to svg container
-group
-  .append("rect")
-  .attr("height", 100)
-  .attr("width", 200)
-  .attr("fill", "blue")
-  .attr("x", 20)
-  .attr("y", 20);
-
-group
-  .append("circle")
-  .attr("r", 50)
-  .attr("cx", 300)
-  .attr("cy", 75)
-  .attr("fill", "pink");
-
-group
-  .append("line")
-  .attr("x1", 370)
-  .attr("x2", 400)
-  .attr("y1", 20)
-  .attr("y2", 120)
-  .attr("stroke", "red");
-
-svg
-  .append("text")
-  .attr("x", 20)
-  .attr("y", 200)
-  .attr("fill", "grey")
-  .text("Hello World")
-  .style("font-familt", "arial");
-// style give access to css styles
+const rect = svg
+  .select("rect")
+  .data(data)
+  .attr("width", (d) => d.width)
+  .attr("height", (d) => d.height)
+  .attr("fill", (d) => d.fill);
