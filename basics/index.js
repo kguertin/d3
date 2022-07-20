ccb697d3c6297b179767b43852c6b7244e794752
Kevin Guertin <kevin.m.guertin@gmail.com>
2022-07-20 14:07:53 -0400

    pull in data from external file

--- basics/index.js
+++ basics/index.js
@@ -1,23 +1,18 @@
-const data = [
-  { width: 200, height: 100, fill: "purple" },
-  { width: 100, height: 60, fill: "pink" },
-  { width: 50, height: 30, fill: "red" },
-];
-
 const svg = d3.select("svg");
 
-const rects = svg.selectAll("rect").data(data);
+d3.json("planets.json").then((data) => {
+  const circs = svg.selectAll("circle").data(data);
+  circs
+    .attr("cy", 200)
+    .attr("cx", (d) => d.distance)
+    .attr("r", (d) => d.radius)
+    .attr("fill", (d) => d.fill);
 
-// Add attr to rects in DOM
-rects
-  .attr("width", (d) => d.width)
-  .attr("height", (d) => d.height)
-  .attr("fill", (d) => d.fill);
-
-// Append the enter selection to DOM
-rects
-  .enter()
-  .append("rect")
-  .attr("width", (d) => d.width)
-  .attr("height", (d) => d.height)
-  .attr("fill", (d) => d.fill);
+  circs
+    .enter()
+    .append("circle")
+    .attr("cy", 200)
+    .attr("cx", (d) => d.distance)
+    .attr("r", (d) => d.radius)
+    .attr("fill", (d) => d.fill);
+});
