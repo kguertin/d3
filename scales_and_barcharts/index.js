bd5420139820f8b05c2f35d7afb9e8c8a993dd3f
Kevin Guertin <kevin.m.guertin@gmail.com>
2022-07-21 14:52:37 -0400

    make bar chart

--- scales_and_barcharts/index.js
+++ scales_and_barcharts/index.js
@@ -1,24 +1,77 @@
-const svg = d3.select("svg");
+const svg = d3
+  .select(".canvas")
+  .append("svg")
+  .attr("width", 600)
+  .attr("height", 600);
+
+// Create margins and dimensions
+const margin = {
+  top: 20,
+  right: 20,
+  bottom: 100,
+  left: 100,
+};
+const graphWidth = 600 - margin.left - margin.right;
+const graphHeight = 600 - margin.top - margin.bottom;
+
+const graph = svg
+  .append("g")
+  .attr("graphWidth", graphWidth)
+  .attr("graphHeight", graphHeight)
+  .attr("transform", `translate(${margin.left},${margin.top})`);
+
+const xAxisGroup = graph
+  .append("g")
+  .attr("transform", `translate(0, ${graphHeight})`);
+const yAxisGroup = graph.append("g");
 
 d3.json("menu.json").then((data) => {
-  const y = d3.scaleLinear().domain([0, 1000]).range([0, 500]);
+  const y = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => d.orders)])
+    .range([graphHeight, 0]);
+
+  const x = d3
+    .scaleBand()
+    .domain(data.map((i) => i.name))
+    .range([0, 500])
+    .paddingInner(0.2)
+    .paddingOuter(0.2);
 
   // Join data to rects
-  const rects = svg.selectAll("rect").data(data);
+  const rects = graph.selectAll("rect").data(data);
 
   // Join data to rects in DOM
   rects
-    .attr("width", 50)
+    .attr("width", x.bandwidth)
     .attr("height", (d) => y(d.orders))
     .attr("fill", "orange")
-    .attr("x", (d, i) => i * 70);
+    .attr("x", (d) => x(d.name))
+    .attr("y", (d) => y(d.orders));
 
   // append the enter selection to the DOM
   rects
     .enter()
     .append("rect")
-    .attr("width", 50)
-    .attr("height", (d) => y(d.orders))
+    .attr("width", x.bandwidth)
+    .attr("height", (d) => graphHeight - y(d.orders))
     .attr("fill", "orange")
-    .attr("x", (d, i) => i * 70);
+    .attr("x", (d) => x(d.name))
+    .attr("y", (d) => y(d.orders));
+
+  // Create and call the axes
+  const xAxis = d3.axisBottom(x);
+  const yAxis = d3
+    .axisLeft(y)
+    .ticks(3)
+    .tickFormat((d) => d + " orders");
+
+  xAxisGroup.call(xAxis);
+  yAxisGroup.call(yAxis);
+
+  xAxisGroup
+    .selectAll("text")
+    .attr("transform", "rotate(-40)")
+    .attr("text-anchor", "end")
+    .attr("fill", "orange");
 });
